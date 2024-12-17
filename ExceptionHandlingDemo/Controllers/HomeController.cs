3a80be4d9572da05cb89b7424f7fcaf2cc7d9970
Baiketli Dijo <baiketlid@gmail.com>
2024-12-17 17:58:20 +0200

    Status codes handling

--- ExceptionHandlingDemo/Controllers/HomeController.cs
+++ ExceptionHandlingDemo/Controllers/HomeController.cs
@@ -13,8 +13,41 @@ namespace ExceptionHandlingDemo.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? id = null)
         {
+            if (id.HasValue)
+            {
+                if (id == 1)
+                {
+                    throw new FileNotFoundException("File not found exception thrown in index.chtml");
+                }
+                else if (id == 2)
+                {
+                    return StatusCode(500);
+                }
+            }
+            return View();
+        }
+
+        public IActionResult MyStatusCode(int code)
+        {
+            if (code == 404)
+            {
+                ViewBag.ErrorMessage = "The requested page not found.";
+            }
+            else if (code == 500)
+            {
+                ViewBag.ErrorMessage = "My custom 500 error message.";
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "An error occurred while processing your request.";
+            }
+
+            ViewBag.RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            ViewBag.ShowRequestId = !string.IsNullOrEmpty(ViewBag.RequestId);
+            ViewBag.ErrorStatusCode = code;
+
             return View();
         }
 
--- ExceptionHandlingDemo/Program.cs
+++ ExceptionHandlingDemo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,11 +10,39 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    //app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler(errorApp =>
+    {
+        errorApp.Run(async context =>
+        {
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "text/html";
+
+            await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+            await context.Response.WriteAsync("ERROR!<br><br>\r\n");
+
+            var exceptionHandlerPathFeature =
+                context.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+            {
+                await context.Response.WriteAsync(
+                                          "File error thrown!<br><br>\r\n");
+            }
+
+            await context.Response.WriteAsync(
+                                          "<a href=\"/\">Home</a><br>\r\n");
+            await context.Response.WriteAsync("</body></html>\r\n");
+            await context.Response.WriteAsync(new string(' ', 512));
+        });
+    });
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+//app.UseStatusCodePages();
+//app.UseStatusCodePagesWithRedirects("/Home/MyStatusCode?code={0}");
+app.UseStatusCodePagesWithReExecute("/Home/MyStatusCode2", "?code={0}");
 app.UseHttpsRedirection();
 app.UseRouting();
 
